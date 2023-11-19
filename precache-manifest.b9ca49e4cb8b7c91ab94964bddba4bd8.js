77e1076c870f5358b4f5e9579e838a4cd50a1d2b
yaswanthp7799 <yaswanthp7799@gmail.com>
2023-11-20 02:20:32 +0530

    Updates

---
+++ precache-manifest.b9ca49e4cb8b7c91ab94964bddba4bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf71ec44d31a0ac1f839bdfdd1b1e7b",
+    "url": "/Static_GK/index.html"
+  },
+  {
+    "revision": "5cc3a53f62ac91ba0275",
+    "url": "/Static_GK/static/css/main.d9bcc326.chunk.css"
+  },
+  {
+    "revision": "b73f25722bbbcaf4f9d5",
+    "url": "/Static_GK/static/js/2.b7774703.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/Static_GK/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "5cc3a53f62ac91ba0275",
+    "url": "/Static_GK/static/js/main.651a7c9e.chunk.js"
+  },
+  {
+    "revision": "0a13bdf6ea4c3025d406",
+    "url": "/Static_GK/static/js/runtime~main.069a1962.js"
+  }
+]);
